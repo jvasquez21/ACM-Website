9c3e2a4415b90e7db8e3efbadb5db882ae4b0e27
Andrew Hutton <fromalaska49@gmail.com>
2017-10-23 10:54:54 -0500

    Meeting Customer Requirements
    In regards to the lack of content, my apologies to the user…

--- common/menu.php
+++ common/menu.php
@@ -25,6 +25,7 @@
 						<li><a href=".">Home</a></li>
 						<li><a href="about">About</a></li>
 						<li><a href="groups">Groups</a></li>
+						<li><a href="resources">Resources</a></li>
 						<li><a href="shell" id="shell-link">Shell</a></li>
 						<li><a href="officers">Officers</a></li>
 						<li><a href="contact">Contact Us</a></li>
---
+++ resources.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<?php
+			require('common/head-includes.php');
+		?>
+        <title>Resources | Association for Computing Machinery UTSA</title>
+	</head>
+	<body>
+		<?php
+			require('common/menu.php');
+		?>
+		<div id="content" class="container">
+			<div class="row">
+				<div class="col-sm-12">
+					<div class="row section-container">
+						<div class="col-sm-12">
+							We have laptop recommendations <a href="laptops">here</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<?php
+			require('common/footer.php');
+		?>
+	</body>
+</html>
