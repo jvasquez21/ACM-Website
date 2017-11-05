f5c9dde60124af6ff68dbd8fd4c2bfe9296f9f5c
jvasquez21 <juliavasquez69@gmail.com>
2017-11-05 02:21:03 -0600

    Added a popover plugin -Julia Vasquez

--- common/footer.php
+++ common/footer.php
@@ -7,55 +7,55 @@
 			<div class="col-sm-6" id="social-media-container">
 				<div class="row" style="">
 					<div class="hidden-xs col-sm-12">
-						<a href="mailto:acm.utsa@gmail.com" class="footer-social">
+						<a href="mailto:acm.utsa@gmail.com" data-toggle="popover" title="Gmail" class="footer-social">
 							<div class="fa fa-envelope-square fa-3x">
 							</div>
 						</a>
-						<a href="http://acm-utsa.slack.com" class="footer-social">
+						<a href="http://acm-utsa.slack.com" data-toggle="popover" title="Slack" class="footer-social">
 							<div class="fa fa-slack fa-3x">
 							</div>
 						</a>
-						<a href="http://github.com/UTSA-ACM" class="footer-social">
+						<a href="http://github.com/UTSA-ACM" data-toggle="popover" title="Github" class="footer-social">
 							<div class="fa fa-github fa-3x">
 							</div>
 						</a>
-						<a href="http://www.facebook.com/ACM-UTSA-424006151132274/?fref=ts" class="footer-social">
+						<a href="http://www.facebook.com/ACM-UTSA-424006151132274/?fref=ts" data-toggle="popover" title="Facebook" class="footer-social">
 							<div class="fa fa-facebook-square fa-3x">
 							</div>
 						</a>
-						<a href="http://twitter.com/acm_utsa" class="footer-social">
+						<a href="http://twitter.com/acm_utsa" data-toggle="popover" title="twitter" class="footer-social">
 							<div class="fa fa-twitter-square fa-3x">
 							</div>
 						</a>
-						<a href="http://www.linkedin.com/groups/8413611" class="footer-social">
+						<a href="http://www.linkedin.com/groups/8413611" data-toggle="popover" title="Linkedin" class="footer-social">
 							<div class="fa fa-linkedin-square fa-3x">
 							</div>
 						</a>
 					</div>
 					<div class="col-xs-12 hidden-sm hidden-md hidden-lg hidden-xl">
-						<a href="mailto:acm.utsa@gmail.com" class="footer-social">
+						<a href="mailto:acm.utsa@gmail.com" data-toggle="popover" title="Gmail" class="footer-social">
 							<div class="fa fa-envelope-square fa-3x">
 							</div>
 						</a>
-						<a href="http://acm-utsa.slack.com" class="footer-social">
+						<a href="http://acm-utsa.slack.com" data-toggle="popover" title="Slack" class="footer-social">
 							<div class="fa fa-slack fa-3x">
 							</div>
 						</a>
-						<a href="http://github.com/UTSA-ACM" class="footer-social">
+						<a href="http://github.com/UTSA-ACM" data-toggle="popover" title="Github" class="footer-social">
 							<div class="fa fa-github fa-3x">
 							</div>
 						</a>
 					</div>
 					<div class="col-xs-12 hidden-sm hidden-md hidden-lg hidden-xl">
-						<a href="http://www.facebook.com/ACM-UTSA-424006151132274/?fref=ts" class="footer-social">
+						<a href="http://www.facebook.com/ACM-UTSA-424006151132274/?fref=ts" data-toggle="popover" title="Facebook" class="footer-social">
 							<div class="fa fa-facebook-square fa-3x">
 							</div>
 						</a>
-						<a href="http://twitter.com/acm_utsa" class="footer-social">
+						<a href="http://twitter.com/acm_utsa" data-toggle="popover" title="twitter" class="footer-social">
 							<div class="fa fa-twitter-square fa-3x">
 							</div>
 						</a>
-						<a href="http://www.linkedin.com/groups/8413611" class="footer-social">
+						<a href="http://www.linkedin.com/groups/8413611" data-toggle="popover" title="Linkedin" class="footer-social">
 							<div class="fa fa-linkedin-square fa-3x">
 							</div>
 						</a>
